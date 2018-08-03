59442e54effe77344dc692d3f783b808b86172d3
alejandron00 <38502923+alejandron00@users.noreply.github.com>
2018-08-03 10:30:42 -0500

    i change the actionperformed and i add an if

--- src/interfaz/PanelInformacion.java
+++ src/interfaz/PanelInformacion.java
@@ -80,5 +80,9 @@ public class PanelInformacion extends JPanel implements ActionListener{
 		if(comando.equals(GENERAR_MATRIZ)){
 			principal.generarMatriz();			
 		}
+		if(comando.equals(GUARDAR)) {
+			principal.cargar();
+		}
 	}
+
 }
